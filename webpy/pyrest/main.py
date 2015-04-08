01d95e5aac946503a19a986f4e58f2c7e5ff6213
aws-web137 <aws-web137-gm>
2015-04-08 10:20:37 +0000

    20150408 support numjucks (have small bug with share template)

--- webpy/pyrest/main.py
+++ webpy/pyrest/main.py
@@ -13,7 +13,7 @@ import sys
 
 import web
 from web.contrib.template import  render_jinja
-from jinja2 import Environment,FileSystemLoader
+from jinja2 import Environment,FileSystemLoader,ChoiceLoader
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
@@ -34,11 +34,23 @@ render = render_jinja(template_dir, encoding = 'utf-8')
 def render_template(template_name,**context):
     extensions=context.pop('extensions',[])
     globals=context.pop("globals",{})
+    '''
     jinja_env=Environment(
         loader=FileSystemLoader(template_dir),
         extensions=extensions)   
+    '''
+    '''
+    #jinja_dirs = [ jinja2.FileSystemLoader(dirname) for dirname in sub_dirs ]
+    jinja_dirs = [ jiaja2.FileSystemLoader('/static/templates'), jiaja2.FileSystemLoader(template_dir) ]
+    jinja_env = jinja2.Environment (loader = jinja2.ChoiceLoader(jinja_dirs))
+    '''
+    loader = ChoiceLoader([FileSystemLoader(os.path.join(rootdir, 'templates')),
+                                     FileSystemLoader(os.path.join(rootdir, 'static', 'templates'))])
+    jiaja_env = Environment( loader=loader, extensions=extensions)
     jinja_env.globals.update(globals)
+
     return jinja_env.get_template(template_name).render(context)
+
 
 #--------------------------------------------------------------
 #views.py
@@ -51,14 +63,30 @@ class Test:
 class bootst:
     def GET(self):
         return render.bootst()
+
+
+class numj:
+    def GET(self):
+        items = [{
+            'title': 'Facebook',
+            'body': 'Facebook is a social utility that connects people with friends and others who work, study and live around them.'
+        }, {
+            'title': 'Twitter',
+            'body': 'Twitter is an online social networking and microblogging service that enables users to send and read "tweets", which are text messages limited to 140 characters.'
+        }, {
+            'title': 'LinkedIn',
+            'body': 'LinkedIn is a social networking website for people in professional occupations.'
+        }];
+        return render.numj(items=items)
 #--------------------------------------------------------------
 #urls.py
 
 urls = (
-'/api/todos(?:/(?P<todo_id>[0-9]+))?', 'controllers.todos.Todos',
-'/', 'controllers.index.Index',
-'/test', 'Test',
-'/bs', 'bootst'
+'/api/todos(?:/(?P<todo_id>[0-9]+))?', 'controllers.todos.Todos', #test rest
+'/', 'controllers.index.Index',  # test basic url 
+'/test', 'Test',   # test jinja2
+'/bs', 'bootst',   # test bootstrap
+'/nj', 'numj'      # test numjucks  , share same template with jinja2 
 )
 
 
---
+++ webpy/pyrest/static/js/index.js
@@ -0,0 +1,15 @@
+
+nunjucks.configure('/static/templates');
+//nunjucks.configure('/static');
+
+$(function () {
+    $('button[type="submit"]').click(function (evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        $('ul.list-group').append(nunjucks.render('_row.html', {
+            'title': $('#name').val(),
+            'body': $('#description').val()
+        }))
+    });
+});
